0653119af18797c013dce5e1ab3c23affafd2581
gabriel <gabrielcomputacao@outlook.com>
2023-10-26 06:09:35 -0300

    style:  using images with next

--- shop-next/src/pages/_app.tsx
+++ shop-next/src/pages/_app.tsx
@@ -3,13 +3,14 @@ import type { AppProps } from "next/app";
 
 import logoImg from "../assets/logo.svg";
 import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
 
 globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg.src} alt="" width={120} height={120} />
       </Header>
       <Component {...pageProps} />
     </Container>
