0cebf13680a02c95fec6823a614c522e1b507838
gabriel <gabrielcomputacao@outlook.com>
2023-10-31 06:46:59 -0300

    feat:  refatoring product page with routes id

--- shop-next/src/pages/product/[id].tsx
+++ shop-next/src/pages/product/[id].tsx
@@ -3,26 +3,78 @@ import {
   ProductContainer,
   ProductDetails,
 } from "@/styles/pages/product";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+import { stripe } from "../../lib/stripe";
+import Stripe from "stripe";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <p>isLoading...</p>;
+  }
 
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image
+          src={product.imageUrl}
+          alt="imagem Camiseta"
+          height={480}
+          width={520}
+        />
+      </ImageContainer>
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>20,00</span>
-        <p>
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          quia, in, dolores inventore, laboriosam debitis nostrum dignissimos
-          facere saepe labore doloribus eos fugit? Quae, quasi hic? Laudantium
-          debitis voluptatibus repellendus.{" "}
-        </p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
         <button>Comprar Agora</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_OtnTxwLXoHmhcU" } }],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any> = async ({ params }) => {
+  const productId = String(params?.id);
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount ? price.unit_amount / 100 : 10),
+        description: product.description,
+      },
+    },
+  };
+};
