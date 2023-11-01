bc7ac8e051124f995d2cc358b9b9e009ceb9018e
gabriel <gabrielcomputacao@outlook.com>
2023-11-01 06:49:43 -0300

    feat:  creating page success

--- shop-next/src/pages/success.tsx
+++ shop-next/src/pages/success.tsx
@@ -1,3 +1,18 @@
+import { ImageContainerSucces, SucessContainer } from "@/styles/pages/sucess";
+import Link from "next/link";
+
 export default function Success() {
-  return <></>;
+  return (
+    <SucessContainer>
+      <h1>Compra Efetuada!</h1>
+
+      <ImageContainerSucces></ImageContainerSucces>
+
+      <p>
+        Parabens <strong>Gabriel</strong> ! Sua compra foi efetuada com sucesso!
+      </p>
+
+      <Link href="/">Voltar ao Catalogo</Link>
+    </SucessContainer>
+  );
 }
