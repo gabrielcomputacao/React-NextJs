6832fdf9019bbbcd5f2eea971fd91c1f873ac030
gabriel <gabrielcomputacao@outlook.com>
2023-11-02 07:14:13 -0300

    feat:  refactor page success with purchase id

--- shop-next/src/pages/success.tsx
+++ shop-next/src/pages/success.tsx
@@ -1,7 +1,14 @@
 import { ImageContainerSucces, SucessContainer } from "@/styles/pages/sucess";
+import { GetServerSideProps } from "next";
 import Link from "next/link";
+import { stripe } from "../lib/stripe";
 
-export default function Success() {
+interface SucessProps {
+  customerName: string;
+  product: string;
+}
+
+export default function Success({ customerName, product }: SucessProps) {
   return (
     <SucessContainer>
       <h1>Compra Efetuada!</h1>
@@ -9,10 +16,31 @@ export default function Success() {
       <ImageContainerSucces></ImageContainerSucces>
 
       <p>
-        Parabens <strong>Gabriel</strong> ! Sua compra foi efetuada com sucesso!
+        Parabens <strong>{customerName}</strong> ! Sua compra foi efetuada com
+        sucesso!
       </p>
 
       <Link href="/">Voltar ao Catalogo</Link>
     </SucessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {});
+
+  const customerName = session.customer_details?.name;
+
+  return {
+    props: {
+      customerName,
+    },
+  };
+};
